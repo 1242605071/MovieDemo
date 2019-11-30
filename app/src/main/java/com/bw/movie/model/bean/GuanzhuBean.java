e5c1c0877f7dfcbcfc29d4ff533416a6c59e4e90
kongpingping123 <1294042619@qq.com>
2019-11-30 08:33:06 +0800

    jjjjjjjj

---
+++ app/src/main/java/com/bw/movie/model/bean/GuanzhuBean.java
@@ -0,0 +1,11 @@
+package com.bw.movie.model.bean;
+
+public class GuanzhuBean {
+
+    public String director;
+    public String imageUrl;
+    public int movieId;
+    public String name;
+    public double score;
+    public String starring;
+}
