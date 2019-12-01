0f48c2b71c91b5397bc2ef56bf2663b2483b6067
xutao0530 <1248525308@qq.com>
2019-12-01 20:32:27 +0800

    xt

--- app/src/main/java/com/bw/movie/view/adapter/ReleaseAdapter.java
+++ app/src/main/java/com/bw/movie/view/adapter/ReleaseAdapter.java
@@ -1,13 +1,19 @@
 package com.bw.movie.view.adapter;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.bw.movie.R;
 
+import com.bw.movie.model.app.App;
 import com.bw.movie.model.bean.HotBean;
+import com.bw.movie.view.activity.ChangeActivity;
 import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.chad.library.adapter.base.BaseViewHolder;
 
@@ -28,11 +34,22 @@ public class ReleaseAdapter extends BaseQuickAdapter<HotBean,BaseViewHolder> {
     }
 
     @Override
-    protected void convert(BaseViewHolder helper, HotBean item) {
+    protected void convert(BaseViewHolder helper, final HotBean item) {
         helper.setText(R.id.text1,item.score+"分");
         helper.setText(R.id.name,item.name);
         ImageView imageView = helper.getView(R.id.hot_i1);
         imageView.setImageURI(Uri.parse(item.imageUrl));
+        Button payticket = helper.getView(R.id.payticket);
+        payticket.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+                Intent intent = new Intent(App.context, ChangeActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                intent.putExtra("movieId",item.movieId);
+                App.context.startActivity(intent);
+            }
+        });
     }
+
 }
