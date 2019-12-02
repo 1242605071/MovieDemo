90b98bb472af8d20f43c0049fe25fc28ba2ae8dd
xuanran <xuan@123.com>
2019-12-02 09:18:23 +0800

    退出登录

--- app/src/main/java/com/bw/movie/view/myactivity/SetupActivity.java
+++ app/src/main/java/com/bw/movie/view/myactivity/SetupActivity.java
@@ -1,11 +1,14 @@
 package com.bw.movie.view.myactivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 import com.bw.movie.R;
 import com.bw.movie.model.base.BaseActivity;
+import com.bw.movie.view.activity.LoginActivity;
 import com.bw.movie.view.activity.ModifyActivity;
 
 import butterknife.BindView;
@@ -37,6 +40,11 @@ public class SetupActivity extends BaseActivity {
     public void onViewClicked(View view) {
         switch (view.getId()){
             case R.id.but_tuiLogin:
+                SharedPreferences sharedPreferences = getSharedPreferences("login", MODE_PRIVATE);
+                sharedPreferences.edit().clear().commit();
+                Intent intent = new Intent(SetupActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
                 break;
         }
     }
