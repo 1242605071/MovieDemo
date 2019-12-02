9e8cafa0129f47ab2be101d9843353bcb608f476
cao <1242605071@qq.com>
2019-12-02 09:33:57 +0800

    sss

--- app/src/main/java/com/bw/movie/view/myactivity/SetupActivity.java
+++ app/src/main/java/com/bw/movie/view/myactivity/SetupActivity.java
@@ -1,16 +1,20 @@
 package com.bw.movie.view.myactivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.bw.movie.R;
 import com.bw.movie.model.base.BaseActivity;
-import com.bw.movie.view.activity.ModifyActivity;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+
+import static com.baidu.mapapi.BMapManager.getContext;
 
 /**
  * 设置
@@ -20,6 +24,10 @@ public class SetupActivity extends BaseActivity {
 
     @BindView(R.id.but_tuiLogin)
     Button butTuiLogin;
+    @BindView(R.id.genxin)
+    TextView mGenxin;
+    @BindView(R.id.gen)
+    ImageView mGen;
 
     @Override
     protected int initLayout() {
@@ -33,11 +41,19 @@ public class SetupActivity extends BaseActivity {
         ButterKnife.bind(this);
     }
 
-    @OnClick(R.id.but_tuiLogin)
+    @OnClick({R.id.genxin, R.id.gen,R.id.but_tuiLogin})
     public void onViewClicked(View view) {
-        switch (view.getId()){
+        switch (view.getId()) {
             case R.id.but_tuiLogin:
                 break;
+                case R.id.genxin:
+                    Intent intent = new Intent(SetupActivity.this, BanActivity.class);
+                    startActivity(intent);
+                    break;
+                case R.id.gen:
+                    Intent intent6 = new Intent(SetupActivity.this, BanActivity.class);
+                    startActivity(intent6);
+                    break;
         }
     }
 
--- app/src/main/java/com/bw/movie/view/myactivity/TicketActivity.java
+++ app/src/main/java/com/bw/movie/view/myactivity/TicketActivity.java
@@ -1,19 +1,74 @@
 package com.bw.movie.view.myactivity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 import com.bw.movie.R;
+import com.bw.movie.model.base.BaseActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
- *
  * 我的电影票
  */
-public class TicketActivity extends AppCompatActivity {
+public class TicketActivity extends BaseActivity {
+
+
+    @BindView(R.id.back)
+    ImageView mBack;
+    @BindView(R.id.dian)
+    Button mDian;
+    @BindView(R.id.rl)
+    RelativeLayout mRl;
+    @BindView(R.id.piao)
+    ImageView mPiao;
+    @BindView(R.id.quxiao)
+    CardView mQuxiao;
+    @BindView(R.id.but_qu)
+    Button mButQu;
+
+    @Override
+    protected int initLayout() {
+        return R.layout.activity_ticket;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_ticket);
+        // TODO: add setContentView(...) invocation
+        ButterKnife.bind(this);
     }
+
+
+    @OnClick({R.id.back, R.id.dian, R.id.quxiao,R.id.but_qu})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.back:
+                finish();
+                break;
+            case R.id.dian:
+                mRl.setVisibility(View.GONE);
+                mPiao.setVisibility(View.VISIBLE);
+                mQuxiao.setVisibility(View.VISIBLE);
+                break;
+            case R.id.quxiao:
+                mRl.setVisibility(View.VISIBLE);
+                mPiao.setVisibility(View.GONE);
+                mQuxiao.setVisibility(View.GONE);
+                break;
+            case R.id.but_qu:
+                mRl.setVisibility(View.VISIBLE);
+                mPiao.setVisibility(View.GONE);
+                mQuxiao.setVisibility(View.GONE);
+                break;
+        }
+    }
+
+
 }
